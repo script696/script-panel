0a2d73d154a368330313d433c48cd2fcc14f44b7
Nikita <script696@yandex.ru>
2022-12-22 11:44:47 +0300

    feat: add preloader

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as AppRouter } from "./AppRouter/AppRouter";
 export { default as Logo } from "./Logo/Logo";
 export { default as ProtectedRoutes } from "./ProtectedRoutes/ProtectedRoutes";
+export { default as Preloader } from "./Preloader/Preloader";
